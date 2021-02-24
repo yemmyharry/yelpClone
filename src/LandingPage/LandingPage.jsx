97d30ca6b40dd9e4883e0d9925d4e565f191d621
yemmyharry <yemmyharry@gmail.com>
2021-02-24 12:38:55 +0100

    added logo on landing page

--- src/LandingPage/LandingPage.jsx
+++ src/LandingPage/LandingPage.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import TopNav from './TopNav/TopNav'
+import logo from '../assets/yelp.png'
+import styles from '../LandingPage/LandingPage.module.css'
 
 function LandingPage() {
     return (
         <div>
             <TopNav />
+            <span className={styles.container}>
+               <img src={logo} alt="yelp-logo" className={styles.logo}/> 
+            </span>
+            
+
         </div>
     )
 }
