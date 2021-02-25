e0ff2051d2a8e5c10f81aaf4e3ce1d795181703f
yemmyharry <yemmyharry@gmail.com>
2021-02-25 11:53:14 +0100

    added routing capability

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import LandingPage from "./LandingPage/LandingPage";
-
+import {Route, Switch } from 'react-router-dom'
+import SearchResults from './LandingPage/SearchResults/SearchResults'
 
 
 function App() {
   return (
     <div className="App">
-     <LandingPage />
+      <Switch>
+        <Route path="/search" component={SearchResults} />
+        <Route path="/" component={LandingPage} />
+      </Switch>
     </div>
   );
 }
